3b5a819e0ca47147458359c6a4838389773fd3bc
Lydia Vega <lyvegama@gmail.com>
2023-04-26 20:16:12 +0200

    series y pelis páginas hechas

--- src/pages/Peliculas.js
+++ src/pages/Peliculas.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "../data/sample.json";
+const Peliculas = () => {
+  const [pelicula, setPelicula] = useState("");
 
+  const soloPeliculas = () => {
+    const peliculas = data.entries;
 
-const Series = () => {
-  return <div className="series">
+    const soloMovies = peliculas.filter(
+      (element) => element.programType === "movie"
+    );
+    console.log(soloMovies);
+    setPelicula(soloMovies);
+  };
 
-<h1>PELICULAS</h1>
+  return (
+    <div className="Peliculas">
+      <h1>Películas</h1>
 
+      <button onClick={soloPeliculas} value="Ver las películas">Ver las películas</button>
 
-
-
-  </div>;
+      <div>
+        {pelicula.length > 0
+          ? pelicula.map((peli) => (
+              <div key={peli.title}>
+                <p>{peli.title}</p>
+              </div>
+            ))
+          : ""}
+      </div>
+    </div>
+  );
 };
 
-export default Series;
+export default Peliculas;
--- src/pages/Series.js
+++ src/pages/Series.js
@@ -1,11 +1,40 @@
-import React from "react";
+  import React, { useState } from 'react'
+  import data from '../data/sample.json'
 
-const Peliculas = () => {
-  return <div className="peliculas">
+const Series = () => {
+    
+      const [series, setSeries] = useState("");
+  
+      const soloSeries = () => {
+  
+          const series = data.entries
+          const soloBuscaSerie = series.filter((element) => element.programType === "series")
+          console.log(soloBuscaSerie);
+          setSeries(soloBuscaSerie)
+      }
+  
+  
+    return (
+      <div className='series'>
+  <h1>Series</h1>
+      <button onClick={soloSeries} value="Ver las series">Ver las series</button>
+  
+      <div>
+          {series.length > 0 ? series.map((serie) => (
+  
+              <div key={serie.title}>
+  
+                  <p>{serie.title}</p>
+                  
+              </div>
+  
+          )) : ""}
+      </div>
+  
+  
+  </div>
+    )
+  };
 
-<h1>series</h1>
 
-  </div>;
-};
-
-export default Peliculas;
+export default Series;
